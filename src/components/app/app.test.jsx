599858e9150913f06aff1f89b30fc0a88973caee
yuno-qq <ars.qqztsq@gmail.com>
2024-06-25 17:01:26 +0300

    module 5 - fix and add snapshot tests

--- src/components/app/app.test.jsx
+++ src/components/app/app.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import {describe, test, expect} from "@jest/globals"
+import configureStore from "redux-mock-store"
+import {Provider} from 'react-redux'
 
 import movies from "../../mocks/movies.jsx"
 
@@ -9,8 +11,42 @@ import App from "./app.jsx"
 
 describe(`<App> snapshots:`, () => {
   test(`should render main page`, () => {
-    const tree = renderer.create(<App movies={movies} />)
-      .toJSON()
+    const initialState = {
+      genre: {
+        id: `all`,
+        name: `All genres`
+      },
+      movies: [
+        {
+          genre: {
+            id: `comedies`,
+            name: `Comedies`,
+          },
+          imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+          url: `/`,
+          previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+          name: `Красный дракон`
+        },
+        {
+          genre: {
+            id: `comedies`,
+            name: `Comedies`,
+          },
+          imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+          url: `/`,
+          previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+          name: `Молчание ягнят`
+        },
+      ]
+    }
+
+    const store = configureStore()(initialState)
+
+    const tree = renderer.create(
+        <Provider store={store}>
+          <App movies={movies} />
+        </Provider>
+    ).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
---
+++ src/components/genre-tabs/genre-tabs.test.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {describe, test, expect} from "@jest/globals"
+import renderer from 'react-test-renderer'
+
+import GenreTabs from "./genre-tabs.jsx"
+
+
+describe(`<GenreTabs/> snapshots:`, () => {
+  test(`should render first tab active`, () => {
+    const genres = [
+      {
+        id: `all`,
+        name: `All genres`
+      },
+      {
+        id: `crime`,
+        name: `Crime`
+      },
+      {
+        id: `comedy`,
+        name: `Comedy`
+      },
+    ]
+    const tree = renderer.create(<GenreTabs
+      genres={genres}
+      activeGenre={genres[0]}
+      onClickTab={() => {}}
+    />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test(`should render third tab active`, () => {
+    const genres = [
+      {
+        id: `all`,
+        name: `All genres`
+      },
+      {
+        id: `crime`,
+        name: `Crime`
+      },
+      {
+        id: `comedy`,
+        name: `Comedy`
+      },
+    ]
+    const tree = renderer.create(<GenreTabs
+      genres={genres}
+      activeGenre={genres[2]}
+      onClickTab={() => {}}
+    />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
