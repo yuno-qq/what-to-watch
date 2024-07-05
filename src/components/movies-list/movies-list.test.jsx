a749092eff6da75979a87bdfbd7b2d27d6da8874
yuno-qq <ars.qqztsq@gmail.com>
2024-07-05 20:49:54 +0300

    module 6 - add and fix MoviesLists snapshot tests

--- src/components/movies-list/movies-list.test.jsx
+++ src/components/movies-list/movies-list.test.jsx
@@ -6,72 +6,74 @@ import configureStore from 'redux-mock-store'
 
 import MoviesList from './movies-list.jsx'
 
+import withVideoMovie from "../../hocs/with-video-movie/with-video-movie.jsx"
+
+
+const movies = [
+  {
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    url: `/`,
+    previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    name: `Красный дракон`,
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+  },
+  {
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    url: `/`,
+    previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    name: `Молчание ягнят`,
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+  },
+  {
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    url: `/`,
+    name: `Ганнибал`,
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+  },
+  {
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    url: `/`,
+    name: `Восхождение Ганнибала`,
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+  },
+  {
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Черное воскресенье`,
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+  },
+  {
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Кари Мора`,
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+  },
+]
 
 describe(`<MoviesList> snapshots:`, () => {
-  test(`should render movies ["Красный дракон", "Молчание ягнят", "Ганнибал", "Восхождение Ганнибала", "Черное воскресенье", "Кари Мора"]`, () => {
-    const movies = [
-      {
-        imageSrc: `https://placehold.co/280x175/EEE/31343C`,
-        url: `/`,
-        previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-        name: `Красный дракон`,
-        genre: {
-          id: `comedies`,
-          name: `Comedies`,
-        },
-      },
-      {
-        imageSrc: `https://placehold.co/280x175/EEE/31343C`,
-        url: `/`,
-        previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-        name: `Молчание ягнят`,
-        genre: {
-          id: `comedies`,
-          name: `Comedies`,
-        },
-      },
-      {
-        imageSrc: `https://placehold.co/280x175/EEE/31343C`,
-        previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-        url: `/`,
-        name: `Ганнибал`,
-        genre: {
-          id: `comedies`,
-          name: `Comedies`,
-        },
-      },
-      {
-        imageSrc: `https://placehold.co/280x175/EEE/31343C`,
-        previewSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-        url: `/`,
-        name: `Восхождение Ганнибала`,
-        genre: {
-          id: `comedies`,
-          name: `Comedies`,
-        },
-      },
-      {
-        imageSrc: `https://placehold.co/280x175/EEE/31343C`,
-        previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
-        url: `/`,
-        name: `Черное воскресенье`,
-        genre: {
-          id: `comedies`,
-          name: `Comedies`,
-        },
-      },
-      {
-        imageSrc: `https://placehold.co/280x175/EEE/31343C`,
-        previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
-        url: `/`,
-        name: `Кари Мора`,
-        genre: {
-          id: `comedies`,
-          name: `Comedies`,
-        },
-      },
-    ]
-
+  test(`should render correctly`, () => {
     const store = configureStore()({
       movies,
       genre: {
@@ -83,7 +85,28 @@ describe(`<MoviesList> snapshots:`, () => {
 
     const tree = renderer.create(
         <Provider store={store}>
-          <MoviesList movies={movies} showingMoviesCount={4} />
+          <MoviesList renderItems={() => {}} movies={movies} showingMoviesCount={4} />
+        </Provider>
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test(`should render movies ["Красный дракон", "Молчание ягнят", "Ганнибал", "Восхождение Ганнибала", "Черное воскресенье", "Кари Мора"]`, () => {
+    const store = configureStore()({
+      movies,
+      genre: {
+        id: `all`,
+        name: `All genres`,
+      },
+      showingMoviesCount: 4
+    })
+
+    const MoviesListWrapped = withVideoMovie(MoviesList)
+
+    const tree = renderer.create(
+        <Provider store={store}>
+          <MoviesListWrapped movies={movies} showingMoviesCount={4} />
         </Provider>
     ).toJSON()
 
