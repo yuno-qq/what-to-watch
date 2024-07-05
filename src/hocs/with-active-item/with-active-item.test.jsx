09609a89090559a9f006c0d4ea6a28ccfa4a98bf
yuno-qq <ars.qqztsq@gmail.com>
2024-07-05 20:23:45 +0300

    module 6 - add snapsht test for withActiveItem HOC

---
+++ src/hocs/with-active-item/with-active-item.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {describe, test, expect} from "@jest/globals"
+import renderer from 'react-test-renderer'
+
+import withActiveItem from "./with-active-item.jsx"
+
+
+describe(`<withActiveItem> snapshots:`, () => {
+  test(`should render component`, () => {
+    const MockComponent = withActiveItem(() => <div className="MOCK_COMPONENT"></div>)
+
+    const tree = renderer.create(<MockComponent />)
+
+    expect(tree).toMatchSnapshot()
+  })
+})
