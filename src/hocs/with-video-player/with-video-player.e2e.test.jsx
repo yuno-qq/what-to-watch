c87cebe00c22b620122ace2ed98b32b072ebefa6
yuno-qq <ars.qqztsq@gmail.com>
2024-07-06 00:21:45 +0300

    module 6 - add e2e test for withVideoPPlayer HOC

---
+++ src/hocs/with-video-player/with-video-player.e2e.test.jsx
@@ -0,0 +1,81 @@
+import React, {act} from "react"
+import Enzyme, {mount} from "enzyme"
+import Adapter from "@cfaester/enzyme-adapter-react-18"
+import {describe, test, expect} from "@jest/globals"
+
+import withVideoPlayer from "./with-video-player.jsx"
+
+
+Enzyme.configure({
+  adapter: new Adapter()
+})
+
+jest.useFakeTimers()
+
+describe(`<withVideoPlayer> change state isPlaying`, () => {
+  test(`small hover + big hover`, () => {
+    const findBlock = () => {
+      block = wrapper.find(`.MOCK_COMPONENT`)
+    }
+
+    const preventDefault = jest.fn()
+
+    const MockComponentWrapped = withVideoPlayer(() => {
+      return <div className="MOCK_COMPONENT"></div>
+    })
+
+    const wrapper = mount(<MockComponentWrapped />)
+
+    let block
+    findBlock()
+
+    wrapper.setProps({
+      isActive: true
+    })
+
+    block.simulate(`mouseenter`, {
+      preventDefault
+    })
+
+    act(() => {
+      jest.advanceTimersByTime(500)
+    })
+
+    expect(wrapper.state(`isPlaying`)).toEqual(false)
+
+    wrapper.setProps({
+      isActive: false
+    })
+
+    findBlock()
+    block.simulate(`mouseleave`, {
+      preventDefault
+    })
+
+    wrapper.setProps({
+      isActive: true
+    })
+
+    findBlock()
+    block.simulate(`mouseenter`, {
+      preventDefault
+    })
+
+    act(() => {
+      jest.advanceTimersByTime(1500)
+    })
+
+    expect(wrapper.state(`isPlaying`)).toEqual(true)
+
+    wrapper.setProps({
+      isActive: false
+    })
+
+    findBlock()
+    block.simulate(`mouseleave`, {
+      preventDefault
+    })
+
+    expect(wrapper.state(`isPlaying`)).toEqual(false)
+  })
+})
