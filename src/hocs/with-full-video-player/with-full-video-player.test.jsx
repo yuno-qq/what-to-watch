4a2056abf1e1a73bfa40b26810236471cdaa5bc0
yuno-qq <ars.qqztsq@gmail.com>
2024-07-23 13:50:15 +0300

    module 6 - add snapshot test for withFullVideoPlayer HOC

---
+++ src/hocs/with-full-video-player/with-full-video-player.test.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import {describe, test, expect} from "@jest/globals"
+import renderer from "react-test-renderer"
+
+import withFullVideoPlayer from "./with-full-video-player.jsx"
+
+
+describe(`withFullVideoPlayer() snapshots`, () => {
+  test(`should render correctly`, () => {
+    const imageSrc = `image.svg`
+    const videoSrc = `movie.avv`
+
+    const MockComponentWrapped = withFullVideoPlayer((props) => <div className="MOCK_COMPONENT">{props.renderItem(imageSrc, videoSrc)}</div>)
+    const tree = renderer.create(<MockComponentWrapped />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
