e27ec743d2cbab9f5108f01012d08af29c7f480d
yuno-qq <ars.qqztsq@gmail.com>
2024-07-23 15:03:24 +0300

    module 6 - add e2e test for withDragging HOC

---
+++ src/hocs/with-dragging/with-dragging.e2e.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Enzyme, {mount} from "enzyme"
+import Adapter from "@cfaester/enzyme-adapter-react-18"
+import {describe, test, expect} from "@jest/globals"
+
+import withDragging from "./with-dragging.jsx"
+
+
+Enzyme.configure({
+  adapter: new Adapter()
+})
+
+describe(`withDragging() onDrag`, () => {
+  test(`should change dragValue`, () => {
+    const MockComponentWrapped = withDragging((props) => <div>
+      <button onClick={props.onDrag(5)}></button>
+    </div>)
+
+    const wrapper = mount(<MockComponentWrapped />)
+    const btn = wrapper.find(`button`)
+
+    btn.simulate(`click`)
+
+    expect(wrapper.state(`dragValue`)).toBe(5)
+  })
+})
