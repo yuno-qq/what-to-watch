e4b2955e50ae1192409e174025b415f8a5ab3f37
yuno-qq <ars.qqztsq@gmail.com>
2024-07-23 15:54:24 +0300

    module 6 - add snapshot test for Btn

---
+++ src/components/btn/btn.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import {describe, test, expect} from "@jest/globals"
+
+import Btn from "./btn.jsx"
+
+
+describe(`<Btn> snapshots`, () => {
+  test(`should render basic case`, () => {
+    const btn = renderer.create(<Btn renderIcon={() => <svg></svg>} title={`Title of button`}/>)
+      .toJSON()
+
+    expect(btn).toMatchSnapshot()
+  })
+
+  test(`should render with all props`, () => {
+    const btn = renderer.create(<Btn renderIcon={() => <svg></svg>}
+      title={`Title of 2nd button`}
+      cssMod={`btn--size_sm`}
+      cssMix={`player__btn`}
+      onClick={() => {}}
+    />).toJSON()
+
+    expect(btn).toMatchSnapshot()
+  })
+})
