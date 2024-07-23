d9dbede5f03c455499e84fbac293a08d8f036e0e
yuno-qq <ars.qqztsq@gmail.com>
2024-07-23 13:57:15 +0300

    module 6 - add snapshot test for withMediaTime HOC

---
+++ src/hocs/with-media-time/with-media-time.test.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {describe, test, expect} from "@jest/globals"
+import renderer from "react-test-renderer"
+
+import withMediaTime from "./with-media-time.jsx"
+
+
+describe(`withMediaTime() snapshots`, () => {
+  test(`should render correctly`, () => {
+    const MockComponentWrapped = withMediaTime(() => <div className="MOCK_COMPONENT"></div>)
+    const tree = renderer.create(<MockComponentWrapped />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
