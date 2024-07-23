66c79ca414558057a8a79b4eb292a6e9f19a5d09
yuno-qq <ars.qqztsq@gmail.com>
2024-07-23 16:21:56 +0300

    module 6 - add snapshot test for PlayBtn

---
+++ src/components/play-btn/play-btn.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import {describe, test, expect} from "@jest/globals"
+
+import PlayBtn from "./play-btn.jsx"
+
+
+describe(`<PlayBtn> snapshots`, () => {
+  test(`should render correctly`, () => {
+    const tree = renderer.create(<PlayBtn
+      onClick={() => {}}
+      cssMix={`player__btn`}
+    />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
