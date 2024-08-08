95fdc38b593ebc18c49c8d54221f5452cd9fb54a
yuno-qq <ars.qqztsq@gmail.com>
2024-08-08 18:40:57 +0300

    module 7 - add e2e tests for App

---
+++ src/components/app/app.e2e.test.jsx
@@ -0,0 +1,280 @@
+import React, {act} from 'react'
+import Enzyme, {mount} from "enzyme"
+import Adapter from "@cfaester/enzyme-adapter-react-18"
+import {describe, test, expect, beforeEach} from "@jest/globals"
+import {Provider} from "react-redux"
+import MockAdapter from "axios-mock-adapter"
+
+import {store, Operation, api} from "../../reducer"
+
+import App from "../../containers/app"
+
+
+Enzyme.configure({
+  adapter: new Adapter()
+})
+
+const movies = [
+  {
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    url: `/`,
+    videoSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    name: `Красный дракон`
+  },
+  {
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+    id: `111111`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    url: `/`,
+    videoSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    name: `Молчание ягнят`
+  },
+  {
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+    id: `111222`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    url: `/`,
+    name: `Ганнибал`
+  },
+  {
+    genre: {
+      id: `comedies`,
+      name: `Comedies`,
+    },
+    id: `111333`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    url: `/`,
+    name: `Восхождение Ганнибала`
+  },
+  {
+    genre: {
+      id: `crime`,
+      name: `Crime`,
+    },
+    id: `111444`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Черное воскресенье`
+  },
+  {
+    genre: {
+      id: `crime`,
+      name: `Crime`,
+    },
+    id: `111555`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Кари Мора`
+  },
+  {
+    genre: {
+      id: `documentary`,
+      name: `Documentary`,
+    },
+    id: `111666`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Профайлер`
+  },
+  {
+    genre: {
+      id: `documentary`,
+      name: `Documentary`,
+    },
+    id: `111777`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Ящик скиннера`
+  },
+  {
+    genre: {
+      id: `documentary`,
+      name: `Documentary`,
+    },
+    id: `111888`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Клинок молчания`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `111999`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Азазель`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222111`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Турецкий гамбит`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222222`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Лефиафан`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222333`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Смерть Ахиллеса`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222444`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Особые поручения`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222555`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Статский советник`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222666`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Коронация`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222777`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Любовница смерти`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222888`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Любовник смерти`
+  },
+  {
+    genre: {
+      id: `dramas`,
+      name: `Dramas`,
+    },
+    id: `222999`,
+    imageSrc: `https://placehold.co/280x175/EEE/31343C`,
+    videoSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    url: `/`,
+    name: `Алмазная колесница`
+  },
+]
+
+beforeEach(() => {
+  store.dispatch({
+    type: `FULL_RESET`
+  })
+})
+
+describe(`<App> load`, () => {
+  test(`should load movies`, () => {
+    mount(
+        <Provider store={store}>
+          <App />
+        </Provider>)
+
+    const loadMovies = Operation.loadMovies()
+
+    const mockApi = new MockAdapter(api)
+    mockApi.onGet(`/c175f975-dcd9-4565-8bc9-05cad0f07a68`)
+      .reply(200, movies)
+
+    return act(() => {
+      return loadMovies(store.dispatch, store.getState, api)
+        // eslint-disable-next-line max-nested-callbacks
+        .then(() => {
+          expect(store.getState().hasServerError).toBe(false)
+          expect(store.getState().movies).toEqual(movies)
+        })
+    })
+  })
+
+  test(`should not load movies`, () => {
+    mount(
+        <Provider store={store}>
+          <App />
+        </Provider>)
+
+    const loadMovies = Operation.loadMovies()
+
+    const mockApi = new MockAdapter(api)
+    mockApi.onGet(`/c175f975-dcd9-4565-8bc9-05cad0f07a68`)
+      .reply(404, null)
+
+    return act(() => {
+      expect.assertions(2)
+      return loadMovies(store.dispatch, store.getState, api)
+        // eslint-disable-next-line max-nested-callbacks
+        .catch(() => {
+          expect(store.getState().hasServerError).toBe(true)
+          expect(store.getState().movies).toEqual([])
+        })
+    })
+  })
+})
+
